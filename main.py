38e38cd92ccc32f0a94e540ea66c102082573667
Eren <vignesh13006@gmail.com>
2025-10-21 14:49:12 +0530

    Intial commit

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print('hellow')
-print('hello bitch')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
